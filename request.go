12f552349f6e2a3f2393eeacb18a6da4f4ae5d12
Peter Hellberg <peter@c7.se>
2025-11-06 16:06:59 +0100

    request: Add RequestOption (and Variable, Header)

--- request.go
+++ request.go
@@ -12,27 +12,48 @@ type Request struct {
 	Header http.Header
 }
 
-// NewRequest makes a new Request with the specified string.
-func NewRequest(q string) *Request {
+// RequestOption is an option function for a request.
+type RequestOption func(*Request)
+
+// Variable sets the provided key and value as a variable on the request.
+func Variable(key string, value any) RequestOption {
+	return func(req *Request) {
+		req.Var(key, value)
+	}
+}
+
+// Header sets the provided key and value as a header on the request.
+func Header(key, value string) RequestOption {
+	return func(req *Request) {
+		req.Header.Set(key, value)
+	}
+}
+
+// NewRequest makes a new request with the specified query and options.
+func NewRequest(q string, options ...RequestOption) *Request {
 	req := &Request{
 		q:      q,
 		Header: make(map[string][]string),
 	}
 
+	for _, option := range options {
+		option(req)
+	}
+
 	return req
 }
 
-// Var sets a variable.
-func (req *Request) Var(key string, value interface{}) {
+// Var sets a variable on this request.
+func (req *Request) Var(key string, value any) {
 	if req.vars == nil {
-		req.vars = make(map[string]interface{})
+		req.vars = make(map[string]any)
 	}
 
 	req.vars[key] = value
 }
 
-// Vars gets the variables for this Request.
-func (req *Request) Vars() map[string]interface{} {
+// Vars gets the variables for this request.
+func (req *Request) Vars() map[string]any {
 	return req.vars
 }
 
--- request_test.go
+++ request_test.go
@@ -5,20 +5,27 @@ import "testing"
 func TestNewRequest(t *testing.T) {
 	q := "query { test }"
 
-	r := NewRequest(q)
+	r := NewRequest(q,
+		Variable("qux", 123),
+		Header("sit", "amet"),
+	)
 
 	if got, want := r.Query(), q; got != want {
 		t.Fatalf("r.Query() = %q, want %q", got, want)
 	}
 
-	if got, want := len(r.Vars()), 0; got != want {
+	if got, want := len(r.Vars()), 1; got != want {
 		t.Fatalf("len(r.Vars()) = %d, want %d", got, want)
+	}
+
+	if got, want := len(r.Header), 1; got != want {
+		t.Fatalf("len(r.Header) = %d, want %d", got, want)
 	}
 
 	r.Var("foo", 123)
 	r.Var("bar", true)
 
-	if got, want := len(r.Vars()), 2; got != want {
+	if got, want := len(r.Vars()), 3; got != want {
 		t.Fatalf("len(r.Vars()) = %d, want %d", got, want)
 	}
 
